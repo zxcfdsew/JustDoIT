8d472f7fe407231bc3e5ff3bbf7f3c4aba7cff45
LEE <seonghi1313@naver.com>
2023-11-05 23:14:53 +0900

    로그인 기능 추가

--- app/src/main/java/com/example/justdoit/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/justdoit/fragment/HomeFragment.kt
@@ -7,12 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.justdoit.R
 import com.example.justdoit.databinding.FragmentHomeBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 
 class HomeFragment : Fragment() {
 
     private var mBinding : FragmentHomeBinding? = null
     private val binding get() = mBinding!!
+
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +37,8 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        mAuth = Firebase.auth
+        binding.testUidTxt.text = mAuth.currentUser!!.uid
 
 
     }
