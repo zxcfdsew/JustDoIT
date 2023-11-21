4d9c137e6e86797d35e7370de01ef71ff92ff90e
seong <seonghi1313@naver.com>
2023-11-21 18:18:29 +0900

    merged

--- app/src/main/java/com/example/justdoit/datas/Community.kt
+++ app/src/main/java/com/example/justdoit/datas/Community.kt
@@ -1,4 +1,4 @@
 package com.example.justdoit.datas
 
-data class Community(val title:String? = null, val content:String? = null, val time:String? = null, val category:String?= null,val documentUid:String?= null, val uid:String? = null ,val heartCount:String? = null,  val commentCount:String? = null,val sadCount:String? = null) :java.io.Serializable{
+data class Community(val title:String? = null, val content:String? = null, val time:String? = null, val category:String?= null,val documentUid:String?= null, val uid:String? = null ,val heartCount:String? = null,  val commentCount:String? = null,val sadCount:String? = null) :java.io.Serializable{}
 
